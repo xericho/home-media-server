12b12e207d17c5a940da7363882841bd695cc5fb
Eric Ho <23301222+xericho@users.noreply.github.com>
2024-09-20 13:11:23 -0700

    updated docker compose files

--- update_containers.sh
+++ update_containers.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-sudo docker-compose pull
-sudo docker-compose up --force-recreate --build -d
+sudo docker compose pull
+sudo docker compose up --force-recreate --build -d
 sudo docker image prune -f
